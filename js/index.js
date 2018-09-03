25852be15f594cc48254d64494b7a55cbfc13615
TheCuriousMeowQ <15728382987@163.com>
2018-09-03 23:19:46 +0800

    to

--- js/index.js
+++ js/index.js
@@ -25,3 +25,22 @@ function scroll() {
         left: document.body.scrollLeft
     }
 }
+
+// 播放mp3
+function audioplay() {
+    if (Miss.paused) {
+        Miss.play();
+    } else {
+        Miss.pause();
+    }
+}
+
+// 数字时间
+// 定义一个函数用以显示当前时间
+function displayTime() {
+    var elt = getId("timer_txt");
+    var now = new Date(); // 得到当前时间
+    elt.innerHTML = now.toLocaleTimeString(); //让elt来显示它
+    setTimeout(displayTime, 1000); //在1秒后再次执行
+}
+// window.onload = displayTime; //当onload事件发生时开始显示时间
